546d479472f42da9f4bfe8fee05c61945af22a26
Bocchi-Dev <47100935+Bocchi-Dev@users.noreply.github.com>
2021-10-16 22:37:23 +0800

    found that edit text broke app for some reason

--- app/src/main/java/com/demo/vendingmachine/MainActivity.java
+++ app/src/main/java/com/demo/vendingmachine/MainActivity.java
@@ -1,6 +1,8 @@
 package com.demo.vendingmachine;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //EditText enteredAmount = (EditText) findViewById(R.id.tvuseramount);
+
+        Button insertMoney = (Button) findViewById(R.id.btninsertmoney);
+        Button buyCoke = (Button) findViewById(R.id.btncoke);
+        Button buySprite = (Button) findViewById(R.id.btnsprite);
+        Button buyIcedTea = (Button) findViewById(R.id.btnicedtea);
+        Button buyRoyal = (Button) findViewById(R.id.btnroyal);
     }
 }
