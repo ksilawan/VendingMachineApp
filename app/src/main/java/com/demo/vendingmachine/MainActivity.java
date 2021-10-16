a9397a3d63614022059d5600d9c681e3b03abd3e
Bocchi-Dev <47100935+Bocchi-Dev@users.noreply.github.com>
2021-10-17 01:44:57 +0800

    tried adding toast but app keeps breaking for some reason

--- app/src/main/java/com/demo/vendingmachine/MainActivity.java
+++ app/src/main/java/com/demo/vendingmachine/MainActivity.java
@@ -1,24 +1,45 @@
 package com.demo.vendingmachine;
 
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //EditText enteredAmount = (EditText) findViewById(R.id.tvuseramount);
+        Context context = getApplicationContext();
+
+        EditText enteredAmount = (EditText) findViewById(R.id.tvuseramount);
 
         Button insertMoney = (Button) findViewById(R.id.btninsertmoney);
         Button buyCoke = (Button) findViewById(R.id.btncoke);
         Button buySprite = (Button) findViewById(R.id.btnsprite);
         Button buyIcedTea = (Button) findViewById(R.id.btnicedtea);
         Button buyRoyal = (Button) findViewById(R.id.btnroyal);
+
+        //buyCoke.setEnabled(false);
+
+        buyCoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Enjoy your coke", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
